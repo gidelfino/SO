92f867a8163b5863eed491b8d960671547ba7d5c
Thomas Ryu <thomasryusameshima@gmail.com>
2016-08-27 00:00:09 -0300

    Embelezamento do código e correção do chmod.

--- ep1sh.c
+++ ep1sh.c
@@ -1,64 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <sys/stat.h>
 
 #define MAX_SIZE 	1024
 
-int main() {
+int main()
+{
 	int  i, status;
     char path[MAX_SIZE], prompt[MAX_SIZE];
-	char *input, *token, *command;
-    char *parameters[MAX_SIZE];
+	char *input, *token, *comnd;
+    char *param[MAX_SIZE];
 	pid_t pid;
 
-    while (42) {	// inicio do shell
+	/* Inicio do shell */
+    while (42) {
     	if (getcwd(path, sizeof(path)) != NULL) {
     		sprintf(prompt, "(%s): ", path);
-    		input = readline(prompt); // impressao do prompt leitura do input do usuario 
-			if (input != NULL) {		  // adicao do input ao historico
-				if (strcmp(input, "") == 0) 
-					continue; // se o input for vazio, pular para a proxima iteracao
+    		input = readline(prompt);
+
+    		/* Se o input nao for vazio, adiciona-lo ao historico */
+			if (input != NULL) {
+				if (strcmp(input, "") == 0) continue;
 				add_history(input);
+
+				/* Tokenizacao do input entre comando e parametros */
 				token = strtok(input, " ");
-				command = token;	  // separacao do input entre comando e parametros
-				parameters[0] = token;
+				comnd = token;
+				param[0] = token;
 				for (i = 1; token != NULL; i++) {
 					token = strtok(NULL, " ");
-					parameters[i] = token;
+					param[i] = token;
 				}
 			}
-			else 
-				perror("readline()");
+			else perror("readline()");
 		}
- 		else
- 			perror("getcwd()");
+ 		else perror("getcwd()");
 
- 		if (strcmp(command, "chmod") == 0) {     // chmod <modo numerico> <nome do diretorio atual>
- 			printf("%s %s\n", parameters[1], parameters[2]);
- 			if (chmod(parameters[2], parameters[1]) != 0)
- 				perror("chmod()");
- 		}
- 		else if (strcmp(command, "id") == 0) {
- 			if (strcmp(parameters[1], "-u") == 0)
+ 		/* id -u */
+ 		if (strcmp(comnd, "id") == 0) {
+ 			if (strcmp(param[1], "-u") == 0)
  				printf("%d\n", (int)getuid());		
  		}
- 		else                                  // /bin/ls -1     e    ./ep1 <argumentos do EP1>
+
+ 		/* chmod <modo numerico> <arquivo no diretorio atual> */
+ 		else if (strcmp(comnd, "chmod") == 0) {
+ 			if (chmod(param[2], strtol(param[1], NULL, 8)) != 0)
+ 				perror("chmod()");
+ 		}
+
+ 		/* /bin/ls -1    /bin/date    ./ep1 <argumentos do EP1> */
+ 		else                      
  			switch (pid = fork()) {
-				case -1: // erro
+				case -1: // Erro
 					perror("fork()");
 					exit(EXIT_FAILURE);
-				case 0:  // estamos em um processo filho
-					status = execve(command, parameters, 0);
+				case 0:  // Processo filho
+					status = execve(comnd, param, 0);
 					exit(status);
-				default: // estamos em um processo pai 
+				default: // Processo pai 
 					if ((waitpid(pid, &status, 0)) < 0) {
 						perror("waitpid()");
 						exit(EXIT_FAILURE);
 					}
 			}
+			
 		free(input);
 	}
 	return 0;
