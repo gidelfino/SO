31cd0614b05d54bf042984730288f5e2fdb21423
gidelfino <giovana.delfino@gmail.com>
2016-09-10 17:57:57 -0300

    Gerador atualizado

--- tests/GenCSV.py
+++ tests/GenCSV.py
@@ -2,24 +2,26 @@ import csv
 import os
 
 for exp in range(3, 4): #Experimento P, M ou G
+	
 	td = open('td' + str(exp) + '.csv', 'w');
 	tc = open('tc' + str(exp) + '.csv', 'w');
 	datad = open('datad' + str(exp), 'w');
 	datac = open('datac' + str(exp), 'w');
-	n = 1
+	n = 30
+	
 	for esc in range(1, 4): #Escalonador 1, 2 ou 3
 
 		sumd = sumc = 0;
-		print("Escalonador " + str(esc))
+		print("Iniciando testes no escalonador " + str(esc))
 		datad.write('Escalonador '+ str(esc) +'---------------------------------' + "\n")
 		datac.write('Escalonador '+ str(esc) +'---------------------------------' + "\n")	
 		if (esc != 3):
 			continue;
+		
 		for i in range(0, n):
-			print("Teste " + str(i) + " com " + str(exp * 10))
+			print("Teste " + str(i) + " com size " + str(exp * 10))
 			os.system("java Generator " + str(exp * 10) + " 5 10 5 > in")
-			os.system("cat in")
-			os.system(".././ep1 " + str(esc) + " in out d")
+			os.system(".././ep1 " + str(esc) + " in out")
 			out = open('out', 'r')
 			for row in out: 
 				continue
